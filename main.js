2fa8a815a27c6794fcf2fd7195498cb977ca8c65
Loidpop2 <LoidpopGames@gmail.com>
2025-09-11 13:25:24 -0700

    fixes!

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 function hotbar_backgroundSelector() {
+    var chosen = false;
     for (var i = 0; i < document.getElementById("hotbar").childElementCount; i++) {
         e = document.getElementsByClassName("hotbar-button")[i];
         if (document.URL.includes(e.getAttribute("href")))
         {
             e.className = "hotbar-button-this";
+            chosen = true;
         }
         console.log(i);
         console.log(e.getAttribute("href"));
         console.log(e);
     }
+    if (chosen == false) { document.getElementsByClassName("hotbar-button")[0].className = "hotbar-button-this"; }
 }
